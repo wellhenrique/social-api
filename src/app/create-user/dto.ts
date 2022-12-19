bf2d26dccc5b0f78bace298a69a7c6f6bd48ff90
wellhenrique <welissonh80@gmail.com>
2022-12-19 19:32:39 -0300

    feat: ensures that exec method exists

---
+++ src/app/create-user/dto.ts
@@ -0,0 +1,5 @@
+export interface CreateUserInput {
+  name: string
+  email: string
+  password: string
+}
--- src/app/create-user/index.spec.tsx
+++ src/app/create-user/index.spec.tsx
@@ -1,9 +1,21 @@
 import { UseCase } from '@/shared/contracts'
+import { CreateUserInput } from './dto'
 
-class CreateUserUseCase {}
+class CreateUserUseCase implements UseCase<CreateUserInput, void> {
+  execute(input: CreateUserInput): Promise<void> {
+    console.log(input)
+    return Promise.resolve()
+  }
+}
 
 describe('CreateUserUseCase', () => {
   it('should be defined', () => {
     expect(new CreateUserUseCase()).toBeDefined()
+  })
+
+  it('should be a execute method', () => {
+    const useCase = new CreateUserUseCase()
+
+    expect(useCase.execute).toBeDefined()
   })
 })
