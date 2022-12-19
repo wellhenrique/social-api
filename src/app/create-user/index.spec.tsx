a088b2c945393c5ab162cab005dbf3ccf83fd49b
wellhenrique <welissonh80@gmail.com>
2022-12-19 19:13:04 -0300

    feat: ensures that CreateUserUseCase to be defined

---
+++ src/app/create-user/index.spec.tsx
@@ -0,0 +1,9 @@
+import { UseCase } from '@/shared/contracts'
+
+class CreateUserUseCase {}
+
+describe('CreateUserUseCase', () => {
+  it('should be defined', () => {
+    expect(new CreateUserUseCase()).toBeDefined()
+  })
+})
